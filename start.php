8660eb8b5a108f40dc293030d5de241fa6ca097f
René Vögeli <rvoegeli@rangee.com>
2019-06-03 14:45:12 +0200

    Move rangee.css

--- start.php
+++ start.php
@@ -33,6 +33,7 @@ elgg_register_event_handler('init', 'system', function () {
 	elgg_unregister_plugin_hook_handler('register', 'menu:login', '_elgg_login_menu_setup');
 
 	elgg_extend_view('elgg.css', 'bulma.css', 1);
+	elgg_extend_view('elgg.css', 'rangee.css', 1);
 	elgg_extend_view('admin.css', 'bulma.css', 1);
 	elgg_extend_view('elgg.js', 'page/elements/topbar.js');
 
