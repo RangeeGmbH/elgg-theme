e40cbc561fc52d470ad46e4252e28393429c1266
René Vögeli <rvoegeli@rangee.com>
2019-08-30 11:51:22 +0200

    Add email signature

--- languages/de.php
+++ languages/de.php
@@ -1,4 +1,94 @@
 <?php
 return [
-  'event_manager:owner:created' => 'Erstellt',
+    'event_manager:owner:created' => 'Erstellt',
+
+    'useradd:body' => '
+%s,
+
+Auf der Community-Seite %s wurde ein Benutzeraccount für Dich erstellt. Um Dich anzumelden, gehe zu:
+
+%s
+
+und melde Dich mit diesen Zugangsdaten an:
+
+Benutzername (Username): %s
+Passwort: %s
+
+Nachdem Du Dich angemeldet hast, solltest Du Dein Passwort ändern.
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'friend:newfriend:body' => 'Du bist nun mit %s befreundet!
+
+Klicke auf den folgenden Link um ihr/sein Profil zu besuchen:
+
+%s
+
+Du kannst auf diese Email NICHT antworten.
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'email:changepassword:body' => 'Hallo %s,
+
+Dein Passwort wurde geändert.
+
+---
+
+Rangee GmbH
+Gut-Dämme-Str. 11
+52070 Aachen
+
+Fon: +49 (0) 241 997 28 50
+Fax: +49 (0) 241 997 28 59
+',
+
+    'email:resetpassword:body' => 'Hallo %s,
+
+Dein Passwort wurde zurückgesetzt. Dein neues Passwort ist: %s
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'email:changereq:body' => 'Hallo %s,
+
+es wurde eine Änderung des Passworts Deines Accounts angefordert (von der IP-Adresse %s).
+
+Falls Du die Änderung des Passworts angefordert hast, klicke bitte auf den folgenden Link, um dies zu bestätigen:
+
+%s
+
+Andernfalls ignoriere bitte diese Email.
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+'
 ];
--- languages/en.php
+++ languages/en.php
@@ -1,61 +1,149 @@
 <?php
 
 return [
-	'menu:page:header:default' => 'Links',
-	'menu:page:header:actions' => 'Quick Links',
-	'menu:page:header:admin' => 'Administer',
-	'menu:page:header:user' => 'User Links',
-	'menu:page:header:group' => 'Group Links',
-	'menu:page:header:danger' => 'Danger Zone',
-	'menu:page:header:notifications' => 'Notifications',
-	'menu:page:header:1_profile' => 'Profile',
-	'menu:page:header:extras' => 'Extras',
-  'menu:page:header:alt' => 'Alt',
+    'menu:page:header:default'       => 'Links',
+    'menu:page:header:actions'       => 'Quick Links',
+    'menu:page:header:admin'         => 'Administer',
+    'menu:page:header:user'          => 'User Links',
+    'menu:page:header:group'         => 'Group Links',
+    'menu:page:header:danger'        => 'Danger Zone',
+    'menu:page:header:notifications' => 'Notifications',
+    'menu:page:header:1_profile'     => 'Profile',
+    'menu:page:header:extras'        => 'Extras',
+    'menu:page:header:alt'           => 'Alt',
 
 
-	'object:menu:no_results' => 'This item has no additional options',
+    'object:menu:no_results' => 'This item has no additional options',
 
-	'user:admin' => 'Administrator',
+    'user:admin' => 'Administrator',
 
-	'blogs:popular' => 'Popular Blogs',
+    'blogs:popular' => 'Popular Blogs',
 
-	'admin:theme' => 'Theme',
-	'admin:theme:assets' => 'Upload Assets',
-	'admin:theme:layout' => 'Layout Settings',
-	'admin:theme:assets:no_files' => 'You must select at least one file to upload',
-	'admin:theme:assets:success' => '%s of %s have been successfully uploaded',
-	'admin:theme:settings_saved' => 'Settings have been saved',
+    'admin:theme'                 => 'Theme',
+    'admin:theme:assets'          => 'Upload Assets',
+    'admin:theme:layout'          => 'Layout Settings',
+    'admin:theme:assets:no_files' => 'You must select at least one file to upload',
+    'admin:theme:assets:success'  => '%s of %s have been successfully uploaded',
+    'admin:theme:settings_saved'  => 'Settings have been saved',
 
-	'admin:theme:asset:topbar_logo' => 'Topbar Logo',
-	'admin:theme:asset:landing_hero' => 'Background of the landing page hero',
+    'admin:theme:asset:topbar_logo'  => 'Topbar Logo',
+    'admin:theme:asset:landing_hero' => 'Background of the landing page hero',
 
-	'admin:theme:layout:site_menu_position' => 'Main Menu Positioning',
-	'admin:theme:layout:site_menu_position:topbar' => 'Topbar (next to the logo)',
-	'admin:theme:layout:site_menu_position:navbar' => 'Underneath Topbar',
+    'admin:theme:layout:site_menu_position'        => 'Main Menu Positioning',
+    'admin:theme:layout:site_menu_position:topbar' => 'Topbar (next to the logo)',
+    'admin:theme:layout:site_menu_position:navbar' => 'Underneath Topbar',
 
-	'admin:theme:layout:site_menu_count' => 'Featured Items',
-	'admin:theme:layout:site_menu_count:help' => 'Maximum number of main menu items to show before adding a More menu item. Set to 0 to display all items without More',
+    'admin:theme:layout:site_menu_count'      => 'Featured Items',
+    'admin:theme:layout:site_menu_count:help' => 'Maximum number of main menu items to show before adding a More menu item. Set to 0 to display all items without More',
 
-	'admin:theme:layout:breadcrumbs' => 'Show breadcrumbs',
+    'admin:theme:layout:breadcrumbs' => 'Show breadcrumbs',
 
-	'admin:theme:layout:sidebar_width' => 'Right Sidebar Width',
-	'admin:theme:layout:sidebar_alt_width' => 'Left Sidebar Width',
+    'admin:theme:layout:sidebar_width'     => 'Right Sidebar Width',
+    'admin:theme:layout:sidebar_alt_width' => 'Left Sidebar Width',
 
-	'admin:theme:layout:style:topbar' => 'Topbar Style',
-	'admin:theme:layout:style:navbar' => 'Navbar Style',
-	'admin:theme:layout:style:header' => 'Header Style',
-	'admin:theme:layout:style:landing_hero' => 'Landing Hero Style',
+    'admin:theme:layout:style:topbar'       => 'Topbar Style',
+    'admin:theme:layout:style:navbar'       => 'Navbar Style',
+    'admin:theme:layout:style:header'       => 'Header Style',
+    'admin:theme:layout:style:landing_hero' => 'Landing Hero Style',
 
-	'admin:theme:layout:landing:title' => 'Landing Page Title',
-	'admin:theme:layout:landing:subtitle' => 'Landing Page Subtitle',
-	'admin:theme:layout:landing:info' => 'Landing Page Info',
-	'admin:theme:layout:landing:module' => 'Landing Page Right Module',
-	'admin:theme:layout:sidebar:before' => 'Sidebar Top',
-	'admin:theme:layout:sidebar:after' => 'Sidebar Bottom',
-	'admin:theme:layout:footer' => 'Footer',
+    'admin:theme:layout:landing:title'    => 'Landing Page Title',
+    'admin:theme:layout:landing:subtitle' => 'Landing Page Subtitle',
+    'admin:theme:layout:landing:info'     => 'Landing Page Info',
+    'admin:theme:layout:landing:module'   => 'Landing Page Right Module',
+    'admin:theme:layout:sidebar:before'   => 'Sidebar Top',
+    'admin:theme:layout:sidebar:after'    => 'Sidebar Bottom',
+    'admin:theme:layout:footer'           => 'Footer',
 
-	'hero:register' => 'Join Our Community!',
-	'core:account:help:register' => 'Not a member yet? %s',
-  'event_manager:owner:created' => 'Created',
-  'profile:maxlength' => 'Max length of %s exceeded at: %s',
+    'hero:register'               => 'Join Our Community!',
+    'core:account:help:register'  => 'Not a member yet? %s',
+    'event_manager:owner:created' => 'Created',
+    'profile:maxlength'           => 'Max length of %s exceeded at: %s',
+
+    'useradd:body' => '
+%s,
+
+A user account has been created for you at %s. To log in, visit:
+
+%s
+
+And log in with these user credentials:
+
+Username: %s
+Password: %s
+
+Once you have logged in, we highly recommend that you change your password.
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'friend:newfriend:body' => '%s has made you a friend!
+
+To view their profile, click here:
+
+%s
+
+Please do not reply to this email.
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'email:changepassword:body' => 'Hi %s,
+
+Your password has been changed.
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'email:resetpassword:body' => 'Hi %s,
+
+Your password has been reset to: %s
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+',
+
+    'email:changereq:body' => 'Hi %s,
+
+Somebody (from the IP address %s) has requested a password change for their account.
+
+If you requested this, click on the link below. Otherwise ignore this email.
+
+%s
+
+---
+
+Rangee GmbH 
+Gut-Dämme-Str. 11 
+52070 Aachen  
+
+Fon:+49 (0) 241 997 28 50 
+Fax: +49 (0) 241 997 28 59 
+'
 ];
